30f4f164b562a13bc814ec23476a3236eaa9dadd
sp00f <e.cudjoe@hotmail.co.uk>
2016-12-13 15:59:37 +0000

    add ffprobe, use to display specs of media file

--- proxybuilder/proxybuilder.py
+++ proxybuilder/proxybuilder.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 from PyQt4 import QtCore, QtGui
 
@@ -13,6 +14,7 @@ class ProxyBuild(QtGui.QWidget):
 
         self.FILETYPES = ('.mov', '.mxf', '.mpg', '.avi')
         self.FFMPEG = '/usr/local/bin/ffmpeg'
+        self.FFPROBE = '/usr/local/bin/ffprobe'
         self.CRF_VALUE = '25'
         self.VIDEO_BR = '100k'
         self.AUDIO_BR = '48k'
@@ -28,7 +30,9 @@ class ProxyBuild(QtGui.QWidget):
         self.build_btn = QtGui.QPushButton('Build')
         self.progress = QtGui.QProgressBar()
         self.progress.setRange(0, 1)
-        self.text_browser = QtGui.QTextBrowser()
+        self.text_browser = QtGui.QPlainTextEdit()
+        self.text_browser.setReadOnly(True)
+        self.text_browser.setWordWrapMode(QtGui.QTextOption.WrapAtWordBoundaryOrAnywhere)
         self.msg = QtGui.QMessageBox()
 
         self.get_target_btn.clicked.connect(self.select_target_dir)
@@ -44,7 +48,6 @@ class ProxyBuild(QtGui.QWidget):
         self.process_proxy.finished.connect(self.process_completed)
         self.scan_process = QtCore.QProcess(self)
         self.scan_process.readyReadStandardError.connect(self.scan_error)
-        self.scan_process.readyReadStandardOutput.connect(self.scan_status)
 
         main_grid = QtGui.QGridLayout()
         main_grid.setSpacing(10)
@@ -54,13 +57,13 @@ class ProxyBuild(QtGui.QWidget):
         main_grid.addWidget(self.scan_btn, 2, 4)
         main_grid.addWidget(self.proxy_location_lbl, 3, 1, 1, 3)
         main_grid.addWidget(self.set_proxy_location_btn, 3, 0)
-        main_grid.addWidget(self.build_btn, 4, 0, 2, 4)
-        main_grid.addWidget(self.progress, 6, 0, 1, 4)
-        main_grid.addWidget(self.text_browser, 7, 0, 10, 6)
+        main_grid.addWidget(self.build_btn, 4, 0, 2, 5)
+        main_grid.addWidget(self.progress, 6, 0, 1, 5)
+        main_grid.addWidget(self.text_browser, 7, 0, 12, 10)
 
         self.setLayout(main_grid)
         self.setGeometry(700, 700, 600, 100)
-        self.setFixedSize(610, 700)
+        self.setFixedSize(1200, 700)
         self.setWindowTitle('Proxy-Builder')
 
     def select_target_dir(self):
@@ -78,9 +81,9 @@ class ProxyBuild(QtGui.QWidget):
     def scan_file(self):
         inpf = self.target_file
         scan_args = [
-            '-i', inpf,
+            '-show_entries', 'stream=index,codec_type,codec_name', inpf,
         ]
-        self.scan_process.start(self.FFMPEG, scan_args)
+        self.scan_process.start(self.FFPROBE, scan_args)
 
     def create_proxy(self):
         orig_file = self.target_file
@@ -89,7 +92,7 @@ class ProxyBuild(QtGui.QWidget):
         arguments = [
             '-progress', 'progress.txt',
             '-i', orig_file,
-            '-y', '-loglevel', 'warning',
+            '-y', '-loglevel', 'verbose',
             '-map', '0:v',
             '-map', '0:0',
             '-map', '0:1',
@@ -107,30 +110,25 @@ class ProxyBuild(QtGui.QWidget):
         proxy_dest = '{}{}'.format(self.proxy_dir, os.path.dirname(os.path.abspath(self.target_file)))
         if not os.path.exists(proxy_dest):
             os.makedirs(proxy_dest)
-        self.text_browser.append(str(arguments))
+
+        self.text_browser.appendPlainText(str(arguments))
         self.process_proxy.start(self.FFMPEG, arguments)
         self.progress.setRange(0, 0)
         self.build_btn.setDisabled(True)
 
     def read_std_error(self):
-        self.text_browser.append(str(self.process_proxy.readAllStandardError()))
+        self.text_browser.appendPlainText(str(self.process_proxy.readAllStandardError()))
 
     def curr_status(self):
-        self.text_browser.append((str(self.process_proxy.readAllStandardOutput()),
-                                  '  '))
-
-    def scan_status(self):
-        self.text_browser.append(('<pre>', str(self.scan_process.readAllStandardOutput()), '</pre>'))
-
+        self.text_browser.appendPlainText(str(self.process_proxy.readAllStandardOutput()))
 
     def scan_error(self):
-        self.text_browser.append(str(self.scan_process.readAllStandardError()))
-
+        self.text_browser.moveCursor(QtGui.QTextCursor.Down)
+        self.text_browser.appendPlainText(str(self.scan_process.readAllStandardError(), 'utf-8'))
 
     def process_completed(self):
         self.progress.setRange(0, 1)
         self.build_btn.setEnabled(True)
-        self.text_browser.clearHistory()
         self.msg.information(self, 'Complete',
                              'Finished conversion')
 
